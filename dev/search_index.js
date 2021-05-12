41761a6dc8ed2876373d72a5a37d7e02579be7ba
Documenter.jl <documenter@juliadocs.github.io>
2021-05-12 20:10:25 +0000

    build based on d0f5489

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Photodynamics","category":"page"},{"location":"#Photodynamics","page":"Home","title":"Photodynamics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Photodynamics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
