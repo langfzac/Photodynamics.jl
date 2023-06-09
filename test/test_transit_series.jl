ae3734647221ecc779388f5473870de1665c676a
Zach Langford <langfzac@uw.edu>
2023-06-09 15:36:21 -0400

    Add derivative methods to test_transit_series

--- test/test_transit_series.jl
+++ test/test_transit_series.jl
@@ -17,6 +17,17 @@ function test_integrator_methods(n)
 
     # Make sure there's no extra computed points
     @test all(pd_computed.points[:,t_range+1:end,:,:] .== 0.0)
+
+    # Now check derivative methods
+    pd_provided = compute_pd(ic, tt, intr, grad=true)
+    pd_computed = compute_pd(ic, intr, grad=true)
+    
+    t_range = length(pd_provided.times)
+    @test pd_provided.times == pd_computed.times[1:t_range]
+    @test pd_provided.bodies == pd_computed.bodies[1:t_range]
+    @test pd_provided.points ≈ pd_computed.points[:,1:t_range,:,:]
+    @test pd_provided.dpoints ≈ pd_computed.dpoints[:,1:t_range,:,:,:,:]
+    @test all(pd_computed.points[:,t_range+1:end,:,:] .== 0.0)
 end
 
 @testset "Integrator Methods" begin
